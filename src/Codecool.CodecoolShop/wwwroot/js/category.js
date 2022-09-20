959489eca680f0ef495b228b8b5b1b98547c5b66
KuraiZoltan <kz1022@gmail.com>
2022-09-20 15:59:32 +0200

    Category change clearing function added

--- src/Codecool.CodecoolShop/wwwroot/js/category.js
+++ src/Codecool.CodecoolShop/wwwroot/js/category.js
@@ -6,8 +6,22 @@ function InIt() {
 
 
 function AddEventListenerToSelect() {
-    const categorySelect = document.querySelector("#select-category")
+    const cardContent = document.querySelector("#product-cards");
+    const categorySelect = document.querySelector("#select-category");
     categorySelect.addEventListener('change', (event) => {
-        console.log(event.currentTarget.value)
+        console.log(event.currentTarget.value);
+        ClearCardContainer(cardContent);
+        
     })
+}
+
+function ClearCardContainer(cardContent) {
+    const bodyContainer = document.querySelector(".pb-3")
+    let newCardContainer = document.createElement("div")
+
+    newCardContainer.classList.add("container")
+    newCardContainer.id = "product-cards"
+
+    cardContent.remove();
+    bodyContainer.appendChild(newCardContainer)
 }
