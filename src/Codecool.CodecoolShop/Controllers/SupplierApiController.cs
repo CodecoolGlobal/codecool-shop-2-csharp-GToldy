8b76f7c00c97046c5720d53951ba74439fe1cea5
KuraiZoltan <kz1022@gmail.com>
2022-09-22 11:14:29 +0200

    Supplier api controller added

---
+++ src/Codecool.CodecoolShop/Controllers/SupplierApiController.cs
@@ -0,0 +1,45 @@
+﻿using Codecool.CodecoolShop.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SupplierApiController : ControllerBase
+    {
+        private readonly ProductService productService;
+
+        public SupplierApiController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
+
+
+        [HttpGet]
+        public List<Product> GetSupplier(int id)
+        {
+            var response = productService.GetProductsForSupplier(id);
+
+            return new List<Product>(response);
+        }
+
+        [HttpGet]
+        public List<Product> GetAllProductsSupplier()
+        {
+            var response = productService.GetProductsForSupplier();
+
+            return new List<Product>(response);
+        }
+
+        [HttpGet]
+        public List<Supplier> GetProductSuppliers()
+        {
+            var response = productService.GetProductSupplier();
+
+            return new List<Supplier>(response);
+        }
+    }
+}
