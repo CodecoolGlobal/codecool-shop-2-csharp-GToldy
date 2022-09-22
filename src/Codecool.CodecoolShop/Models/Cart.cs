1c2bc17ea1187a4fdf9de2ebc6128bd099ee3562
GToldy <georgina.toldy@gmail.com>
2022-09-22 10:31:06 +0200

    Cart item created

---
+++ src/Codecool.CodecoolShop/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class Cart
+    {
+        public List<Item> Items { get; set; }
+
+        public Cart()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
