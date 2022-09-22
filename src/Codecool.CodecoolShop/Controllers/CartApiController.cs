91e17d2912aa875d6d5ca7a1711d1c9b15c45ce5
GToldy <georgina.toldy@gmail.com>
2022-09-22 10:35:10 +0200

    Helper functions created for reading and writing to session

---
+++ src/Codecool.CodecoolShop/Controllers/CartApiController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Codecool.CodecoolShop.Helpers;
+using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
+using Newtonsoft.Json;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    [ApiController]
+    [Route("/api/cart/[action]")]
+    public class CartApiController : Controller
+    {
+        private readonly ProductService _productService;
+        private Cart _cart;
+
+        public CartApiController(ProductService productService, Cart cart)
+        {
+            _productService = productService;
+            _cart = cart;
+        }
+
+        private Cart GetCartFromSession()
+        {
+            return HttpContext.Session.GetObjectFromJson<Cart>("cart");
+        }
+
+        private void SetCartToSession(Cart cart)
+        {
+            HttpContext.Session.SetObjectAsJson("cart", cart);
+        }
+    }
+}
