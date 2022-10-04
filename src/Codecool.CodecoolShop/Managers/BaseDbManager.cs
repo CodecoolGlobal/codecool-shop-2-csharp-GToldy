9c0791b75e73abe17af5b3a584cd588f6ccd6102
KuraiZoltan <kz1022@gmail.com>
2022-10-04 15:52:28 +0200

    Base database manager added

---
+++ src/Codecool.CodecoolShop/Managers/BaseDbManager.cs
@@ -0,0 +1,23 @@
+﻿using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Daos;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace Codecool.CodecoolShop.Managers
+{
+    public class BaseDbManager
+    {
+        private readonly IProductDbDao _productDbDao;
+        private readonly IProductCategoryDbDao _productCategoryDbDao;
+        private readonly ISupplierDbDao _sipplierDbDao;
+        public string connectionString => ConfigurationManager.AppSetting["connectionString"];
+
+        public BaseDbManager(IProductDbDao productDbDao, IProductCategoryDbDao productCategoryDbDao, ISupplierDbDao sipplierDbDao)
+        {
+            _productDbDao = productDbDao;
+            _productCategoryDbDao = productCategoryDbDao;
+            _sipplierDbDao = sipplierDbDao;
+        }
+    }
+}
