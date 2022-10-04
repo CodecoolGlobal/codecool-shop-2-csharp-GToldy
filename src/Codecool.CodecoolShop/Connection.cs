159929463cbb3d780105410687c1383a88ca2202
KuraiZoltan <kz1022@gmail.com>
2022-10-04 15:52:53 +0200

    Database connection class created

---
+++ src/Codecool.CodecoolShop/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Codecool.CodecoolShop
+{
+    public class BookDbManager
+    {
+        public string ConnectionString => ConfigurationManager.["connectionString"];
+        public bool TestConnection()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
