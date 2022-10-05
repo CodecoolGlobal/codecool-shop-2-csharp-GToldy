9af222401fbf37d615959628a19e00d62893d0f6
KuraiZoltan <kz1022@gmail.com>
2022-10-05 16:27:59 +0200

    User's view controller is working

---
+++ src/Codecool.CodecoolShop/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
