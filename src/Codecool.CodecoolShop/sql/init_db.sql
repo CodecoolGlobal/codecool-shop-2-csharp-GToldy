df62fc614f4fac21038d512d748fef72cd80063a
KuraiZoltan <kz1022@gmail.com>
2022-10-05 18:07:49 +0200

    Sql query update

--- src/Codecool.CodecoolShop/sql/init_db.sql
+++ src/Codecool.CodecoolShop/sql/init_db.sql
@@ -35,9 +35,26 @@ CREATE TABLE Cart (
 	user_id INT
 	);
 
+CREATE TABLE Users (
+	id int PRIMARY KEY,
+	name varchar(20),
+	email varchar(30),
+	password varchar(50)
+);
+
+CREATE TABLE PersonalDetail (
+	id int,
+	user_id int,
+	first_name varchar(15),
+	last_name varchar(15),
+	country varchar(30),
+	zipcode int,
+	adress varchar(50),
+);
+
 ALTER TABLE Product ADD FOREIGN KEY (product_category_id) REFERENCES ProductCategory(id);
 ALTER TABLE Product ADD FOREIGN KEY (supplier_id) REFERENCES Supplier(id);
 ALTER TABLE Item ADD FOREIGN KEY (product_id) REFERENCES Product(id);
 ALTER TABLE Item ADD FOREIGN KEY (cart_id) REFERENCES Cart(id);
---ALTER TABLE Cart ADD FOREIGN KEY (user_id) REFERENCES User(id);
---ALTER TABLE PersonalDetail ADD FOREIGN KEY (user_id) REFERENCES User(id);
+ALTER TABLE Cart ADD FOREIGN KEY (user_id) REFERENCES Users(id);
+ALTER TABLE PersonalDetail ADD FOREIGN KEY (user_id) REFERENCES Users(id);
