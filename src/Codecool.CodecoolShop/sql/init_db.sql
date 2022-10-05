f6056046723fb4a11b29eaf08e45339c3f2d53d5
GToldy <georgina.toldy@gmail.com>
2022-10-05 17:07:45 +0200

    Sql tables Cart and Item added as well as foreign keys

--- src/Codecool.CodecoolShop/sql/init_db.sql
+++ src/Codecool.CodecoolShop/sql/init_db.sql
@@ -5,8 +5,8 @@ CREATE TABLE Product (
 	players VARCHAR(10),
 	currency VARCHAR(3),
 	default_price DECIMAL,
-	product_category VARCHAR(36),
-	supplier_category VARCHAR(36),
+	product_category_id INT,
+	supplier_id INT,
 	image VARCHAR(50)
 	);
 
@@ -22,5 +22,22 @@ CREATE TABLE Supplier (
 	description NVARCHAR(MAX)
 	);
 
-ALTER TABLE Product ADD FOREIGN KEY (id) REFERENCES ProductCategory(id);
-ALTER TABLE Product ADD FOREIGN KEY (id) REFERENCES Supplier(id);
+CREATE TABLE Item (
+	id INT PRIMARY KEY,
+	cart_id INT,
+	product_id INT,
+	quantity INT,
+	item_total DECIMAL
+	);
+
+CREATE TABLE Cart (
+	id INT PRIMARY KEY,
+	user_id INT
+	);
+
+ALTER TABLE Product ADD FOREIGN KEY (product_category_id) REFERENCES ProductCategory(id);
+ALTER TABLE Product ADD FOREIGN KEY (supplier_id) REFERENCES Supplier(id);
+ALTER TABLE Item ADD FOREIGN KEY (product_id) REFERENCES Product(id);
+ALTER TABLE Item ADD FOREIGN KEY (cart_id) REFERENCES Cart(id);
+--ALTER TABLE Cart ADD FOREIGN KEY (user_id) REFERENCES User(id);
+--ALTER TABLE PersonalDetail ADD FOREIGN KEY (user_id) REFERENCES User(id);
