13baf85ddfcacd6e3b962f0134b5534f5095cc58
KuraiZoltan <kz1022@gmail.com>
2022-10-07 00:48:33 +0200

    IDaos set to use database

--- src/Codecool.CodecoolShop/Daos/IProductDbDao.cs
+++ src/Codecool.CodecoolShop/Daos/IProductDbDao.cs
@@ -6,8 +6,8 @@ namespace Codecool.CodecoolShop.Daos
 {
     public interface IProductDbDao : IDbDao<Product>
     {
-        IEnumerable<Product> GetBy(Supplier supplier);
-        IEnumerable<Product> GetBy(ProductCategory productCategory);
-        IEnumerable<Product> GetBy(Supplier supplier, ProductCategory productCategory);
+        IEnumerable<Product> GetBySupplier(Supplier supplier);
+        IEnumerable<Product> GetByProductCategory(ProductCategory productCategory);
+        IEnumerable<Product> GetByBoth(Supplier supplier, ProductCategory productCategory);
     }
 }
