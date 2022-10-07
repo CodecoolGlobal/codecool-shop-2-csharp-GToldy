f489fc55e1575a617b21b3202a2dfd68f6b75d02
KuraiZoltan <kz1022@gmail.com>
2022-10-07 03:30:26 +0200

    User controller Register implementation

--- src/Codecool.CodecoolShop/Controllers/UserController.cs
+++ src/Codecool.CodecoolShop/Controllers/UserController.cs
@@ -1,12 +1,20 @@
-﻿using Codecool.CodecoolShop.Models;
+﻿using Codecool.CodecoolShop.Managers;
+using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
+using System.Net.Http;
 using System.Collections.Specialized;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserDbManager _userDbManager;
+
+        public UserController(UserDbManager userDbManager)
+        {
+            _userDbManager = userDbManager;
+        }
+
         public IActionResult Login(User user)
         {
             return View();
@@ -14,7 +22,13 @@ namespace Codecool.CodecoolShop.Controllers
 
         public IActionResult Register(User user)
         {
+            if (HttpContext.Request.Method == HttpMethod.Post.Method)
+            {
+                _userDbManager.RegisterUser(user);
+            }
             return View();
         }
+
+        
     }
 }
